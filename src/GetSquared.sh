fbc8e8c82c357c559dd1a7010ba1ab99f740e86e
Noah Beckerman <noahbeckerman@gmail.com>
2019-07-02 17:50:59 -0400

    Commit Cheat Updated

--- src/GetSquared.sh
+++ src/GetSquared.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 DATE=`date +%Y-%m-%d`
 localPATH=`pwd`                          # path of current directory
-sep='---------------'
+seperator='---------------'
 #for d in */; do
 for d in {1..2}; do
-  echo $sep"Checking Status of" $d$sep
-  d=`echo $d | sed s#/##`                # remove trailing forward slash
-  #git -C $localPATH/$d checkout master   # checkout master in different directory
-  #git -C $localPATH/$d status            # run git status
-  #NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+  d=`echo $d | sed s#/##`
+  echo $seperator"Checking Status of" $d$seperator
   number=$RANDOM
 
   echo $number > $localPATH/GetSquared.html
