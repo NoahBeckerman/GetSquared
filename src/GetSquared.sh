24bf35e4af088eb88cb496b4a12e04167dc1e150
Noah Beckerman <noahbeckerman@gmail.com>
2019-07-03 18:40:59 -0400

    Updated Script

--- src/GetSquared.sh
+++ src/GetSquared.sh
@@ -3,14 +3,19 @@ DATE=`date +%Y-%m-%d`
 localPATH=`pwd`               
            # path of current directory
 seperator='-------------------------'
+echo "How many commits today?"
+read commits
 
-for d in {1..2}; do
+echo  "Pushing [ "$commits" ] to the repo"
+echo -e '\n'
+
+for d in {1..$commits}; do
 
   number=$RANDOM
 
   echo "Printing [ "$number "] to file: ${localPATH/GetSquared.html}"
   echo $number > $localPATH/GetSquared.html
-  
+
   git -C $localPATH/ add . 
   git -C $localPATH/ commit -m "GetSquared: "$number
   git -C $localPATH/ push
