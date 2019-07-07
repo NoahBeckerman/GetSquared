9296cdef1bd64211f5fdb77a1430c5f64498904b
Noah Beckerman <noahbeckerman@gmail.com>
2019-07-07 10:56:15 -0400

    GetSquared: 22533

--- src/GetSquared.sh
+++ src/GetSquared.sh
@@ -12,8 +12,7 @@ echo -e '\n'
 echo  "Pushing ["$commits"] commits to the repo ["$mother_repo"]"
 echo -e '\n'
 
-for x in {seq 1 $commits}
- do
+for x in $(seq 1 $commits); do
 
   number=$RANDOM
 
