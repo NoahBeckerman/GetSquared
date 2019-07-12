fa31a8ae4fa43b6ec3d3bd399644bd9027cd416b
Noah Beckerman <noahbeckerman@gmail.com>
2019-07-12 11:02:42 -0400

    GetSquared: 32152

--- src/GetSquared.sh
+++ src/GetSquared.sh
@@ -3,18 +3,34 @@ localPATH=`pwd`
 mother_repo='http://www.github.com/NoahBeckerman/GetSquared'
 total_repo_commits=$(git rev-list --all --count)
 
+commits_query(){
+	read -p 'How many commits today? ' commits
+	echo -e '\n'
+}
+
+timer_query(){
+	read -p 'How many seconds to sleep? (Recomended is 5 seconds/ Can not go below 2 seconds)' timer
+	echo -e '\n'
+}
+
+commits_query
+timer_query
+
+
+re='^[0-9]+$'
+if ! [[ $commits =~ $re ]] ; then
+   echo "ERROR: Please enter a integer" >&2; exit 1
+fi
+
 echo 'Total ammount of commits made to repo: ' $total_repo_commits
 echo -e '\n'
 
-read -p 'How many commits today? ' commits
-echo -e '\n'
-
-read -p 'How many seconds to sleep? ' timer
-echo "(Recomended is 5 seconds/ Can't go below 2 seconds)"
-echo -e '\n'
 
 echo  "Pushing ["$commits"] commits to the repo ["$mother_repo"]"
 echo -e '\n'
+
+
+
 
 for x in $(seq 1 $commits); do
 
