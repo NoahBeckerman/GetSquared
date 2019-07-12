6625149478ea46637d6de285838df1551f64365b
Noah Beckerman <noahbeckerman@gmail.com>
2019-07-12 13:53:41 -0400

    GetSquared: 26438

--- src/GetForkedUsers.py
+++ src/GetForkedUsers.py
@@ -1,11 +1,12 @@
-import sys, json, requests, pandas as pd
+import sys, json, requests, pandas as pd, getpass
 username = 'NoahBeckerman'
 gh_session = requests.Session()
-gh_session.auth = (username, 'ded6b89bfacc7bc6cb78b9cfd97333c165323300')
+token = getpass.getpass(prompt='Token: ', stream=None)
+gh_session.auth = (username, token)
 json_req = gh_session.get('https://api.github.com/repos/NoahBeckerman/GetSquared/forks')
 users = []
 
 for x in range(len(json_req.json())):
-	users.append(json_req.json()[1]['owner']['login'])
+	users.append(json_req.json()[x]['owner']['login'])
 
 exit(users)
