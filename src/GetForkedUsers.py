91ab4f226e861eaaec5f8b761297618c58be9bff
Noah Beckerman <noahbeckerman@gmail.com>
2019-07-12 13:59:16 -0400

    Updated Code, added python script to list forked users

--- src/GetForkedUsers.py
+++ src/GetForkedUsers.py
@@ -1,9 +1,14 @@
 import sys, json, requests, pandas as pd, getpass
-username = 'NoahBeckerman'
+
 gh_session = requests.Session()
+
+username = getpass.getpass(prompt='Username: ', stream=None)
 token = getpass.getpass(prompt='Token: ', stream=None)
+
 gh_session.auth = (username, token)
 json_req = gh_session.get('https://api.github.com/repos/NoahBeckerman/GetSquared/forks')
+
+
 users = []
 
 for x in range(len(json_req.json())):
